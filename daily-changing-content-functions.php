351fd23a2300f3f0f778b0700cbd23df5bd10c0e
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2018-12-26 18:44:13 +0100

    Plugin header

---
+++ daily-changing-content-functions.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Plugin Name: Daily changing content
+ * Description: Gibt täglich wechselnden Content aus (z.B. Angebot des Tages)
+ * Version: 1.0.0
+ * Author: Peter R. Stuhlmann
+ * Author URI: https://peter-stuhlmann-webentwicklung.de
+ */
