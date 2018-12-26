bd888af19872283931c2df72d3fdfc847056d161
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2018-12-26 23:03:55 +0100

    Variables and switch with empty strings

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+const dcc = new Date()
+
+let DailyChangingContentToday = dcc.getDay()
+
+switch (DailyChangingContentToday) {
+
+    case 0:
+        DailyChangingContent = ``;
+        break;
+
+    case 1:
+        DailyChangingContent = ``;
+        break;
+
+    case 2:
+        DailyChangingContent = ``;
+        break;
+
+    case 3:
+        DailyChangingContent = ``;
+        break;
+
+    case 4:
+        DailyChangingContent = ``;
+        break;
+
+    case 5:
+        DailyChangingContent = ``;
+        break;
+
+    case 6:
+        DailyChangingContent = ``;
+        break;
+}
