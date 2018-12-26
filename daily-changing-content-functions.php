9d034400f22306c4eb9542ce15caad66a6b7b584
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2018-12-26 19:02:10 +0100

    Integrate Stylesheet and Javascript file

--- daily-changing-content-functions.php
+++ daily-changing-content-functions.php
@@ -6,3 +6,12 @@
  * Author: Peter R. Stuhlmann
  * Author URI: https://peter-stuhlmann-webentwicklung.de
  */
+
+
+// Stylesheets and Javascript files
+
+function dailyChangigContent_enqueue_scripts() {
+    wp_enqueue_style( 'daily-changig-content-style', plugin_dir_url( __FILE__ ) . '/assets/css/style.css', '20181226' );
+ 	wp_enqueue_script( 'daily-changig-content-script', plugin_dir_url( __FILE__ ) . '/assets/js/script.js', '20181226' );
+}
+add_action( 'wp_enqueue_scripts', 'dailyChangigContent_enqueue_scripts' );
